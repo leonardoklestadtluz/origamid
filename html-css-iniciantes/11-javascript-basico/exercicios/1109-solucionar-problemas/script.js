f6e2f71dbb51523548129048cb347d5a4928f8ce
leonardoklestadtluz <klestadt@hotmail.com>
2025-04-25 21:37:25 -0300

    html e css para iniciantes - 10 projeto final, conclusão do módulo 11 JavaScript Básico

---
+++ html-css-iniciantes/11-javascript-basico/exercicios/1109-solucionar-problemas/script.js
@@ -0,0 +1,24 @@
+const imagens = document.querySelectorAll('#galeria li img')
+
+function galeriaTrocar(event) {
+  const principal = document.querySelector('#imagem-principal')
+  const clicada = event.currentTarget
+  principal.src = clicada.src
+  principal.alt = clicada.alt
+  console.log(clicada.src)
+  console.log(clicada.alt)
+}
+
+function galeriaClique(imagem) {
+  imagem.addEventListener('click', galeriaTrocar)
+}
+
+imagens.forEach(galeriaClique)
+
+
+
+
+
+
+
+
