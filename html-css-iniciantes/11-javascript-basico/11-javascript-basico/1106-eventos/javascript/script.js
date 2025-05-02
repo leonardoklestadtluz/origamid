f8e5869ae7dcff5edc4059f698fed97a36530e5e
leonardoklestadtluz <klestadt@hotmail.com>
2025-05-02 20:15:16 -0300

    html e css - módulo 11, exclusão de arquivos desnecessários

--- html-css-iniciantes/11-javascript-basico/11-javascript-basico/1106-eventos/javascript/script.js
+++
@@ -1,8 +0,0 @@
-const circulo = document.querySelector("#circulo");
-
-function seguirMouse(event) {
-  circulo.style.top = event.y + "px";
-  circulo.style.left = event.x + "px";
-}
-
-window.addEventListener("mousemove", seguirMouse);
