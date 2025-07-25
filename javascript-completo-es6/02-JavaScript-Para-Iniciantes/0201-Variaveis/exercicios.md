52aed049e565aabc4b8d2ac92183358462212065
leonardoklestadtluz <klestadt@hotmail.com>
2025-07-25 00:32:15 -0300

    javascript completo ES6 - conclusão da aula 0202 Tipos de Dados

---
+++ javascript-completo-es6/02-JavaScript-Para-Iniciantes/0201-Variaveis/exercicios.md
@@ -0,0 +1,7 @@
+Dica rápida: frases(string) devem ser colocadas entre aspas(simples ou duplas) e números não levam aspas
+
+[x] Declarar uma variável com seu nome
+[x] Declarar uma variável com sua idade
+[x] Declarar uma variável com sua comida favorita e não atribuir valor
+[x] Atribuir valor asua comida favorita
+[x] Declarar 5 variáveis diferentes sem valores
---
+++ javascript-completo-es6/02-JavaScript-Para-Iniciantes/0202-Tipos-de-Dados/script.js
@@ -0,0 +1,24 @@
+// Declare uma variável contendo uma string
+var palavra = "texto";
+console.log(palavra);
+
+// Declare uma variável contendo um número dentro de uma string
+var texto = "40";
+console.log(texto);
+
+// Declare uma variável com a sua idade
+var minhaIdade = 40;
+console.log(minhaIdade);
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some(concatenar) as mesmas
+var nome = "Leonardo";
+var sobrenome = "Klestadt Luz";
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contem seu nome
+console.log(typeof nome);
