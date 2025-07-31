0dd7199fc8733c897d2f3a064c0ae316195242df
leonardoklestadtluz <klestadt@hotmail.com>
2025-07-30 23:51:52 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0201 variáveis

---
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0201-variaveis/script.js
@@ -0,0 +1,33 @@
+var nome = "Leonardo";
+var idade = 40;
+possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 10;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobreNome = "Rafael",
+  cidade = "Rio";
+console.log(sobreNome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini);
+
+console.log(comida);
+var comida = "Pizza";
+console.log(comida);
+
+var time1 = "Vasco"; // pode ser alterada, declarada com mesmo nome e redeclarada
+time1 = "flamengo";
+console.log(time1);
+
+let time2 = "Vasco"; // pode ser alterada, mas não declarada com mesmo nome
+time2 = "flamengo";
+console.log(time2);
+
+const time = "Grêmio"; // não pode ser alterada ou redeclarada
+console.log(time);
