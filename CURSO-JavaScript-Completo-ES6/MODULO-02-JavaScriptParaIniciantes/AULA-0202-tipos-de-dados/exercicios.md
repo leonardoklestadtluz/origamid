64de80fd01a09eeb0cc78064aa19b0fcf45a95d0
leonardoklestadtluz <klestadt@hotmail.com>
2025-07-31 22:00:41 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0203 números e operadores 2

---
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0202-tipos-de-dados/exercicios.md
@@ -0,0 +1,8 @@
+- Dica rápida, frases (string) devem ser colocadas entre aspas (simples ou duplas) e números não levam aspas.
+
+[x] Declare uma variável contendo uma string
+[x] Declare uma variável contendo um número dentro de uma string
+[x] Declare uma variável com a sua idade
+[x] Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+[x] Coloque a seguinte frase em uma variável: It's time
+[x] Verifique o tipo da variável que contém o seu nome
---
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0202-tipos-de-dados/script.js
@@ -0,0 +1,28 @@
+var nome = "André";
+var idade = 40;
+var time = null;
+var simbolo = Symbol();
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof time);
+console.log(typeof simbolo);
+
+var nome = "Leonardo ";
+var sobrenome = "Klestadt Luz";
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "Romário fez " + gols + " gols.";
+console.log(typeof frase);
+
+var ano = "2018";
+var mes = 8;
+console.log(ano + mes);
+
+var frase1 = "Esse é o 'melhor' jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols} gols.`;
+console.log(frase2);
