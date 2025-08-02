af13b4ebd834780dae875e5b3dd064d9ed652e42
leonardoklestadtluz <klestadt@hotmail.com>
2025-08-02 19:51:56 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0204 boolean e condicionais

---
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0204-boolean-&-condicionais/script.js
@@ -0,0 +1,129 @@
+// Boolean
+
+// Existem dois valores de booleanos false ou true
+
+// var possuiGraduacao = true
+// var possuiDoutorado = false
+
+// console.log(possuiGraduacao, possuiDoutorado);
+
+// Condicionais if e else
+
+// Verificar se uma expressão é verdadeira com if, caso contrário else será ativado.
+
+// var possuiGraduacao = true
+
+// if(possuiGraduacao) {
+//   console.log('Possui graduação');
+// } else {
+//   console.log('Não possui graduação');
+// }
+
+// var idade = 12 + 47
+// var minhaIdade = 40
+// if(minhaIdade > idade) {
+//   console.log('Minha idade é maior'); 
+// } else {
+//   console.log('Sua idade é maior');
+// }
+
+// console.log(idade);
+
+// retorna Possui graduação e não executa o else
+
+// O valor dentro dos () sempre será avaliado em true ou false
+
+// Condicioais else if
+// var possuiGraduacao = false
+// var possuiDoutorado = true
+
+// if (possuiDoutorado) {
+//   console.log('Possui graduação e doutorado');
+// } else if (possuiGraduacao) {
+//   console.log('Possui graduação, mas não possuí doutorado');
+// } else {
+//   console.log('Não possui graduação');
+// }
+// retorna Possui graduação, mas não possui doutorado
+
+// Truthy e Falsy
+
+// Existem valores que retornam true e outros que retornam false quando verificados em uma expressão booleana
+
+// Falsy
+// if(false)
+// if(0) // ou // 0
+// if(NaN)
+// if(Null)
+// if(undefined)
+// if('') // ou "" ``
+
+// var nome = ''
+// if (nome) {
+//   console.log(nome);
+// } else {
+//   console.log('Nome está vazio');
+// }
+
+// Truthy
+// if(true)
+//   if(1)
+//     if(' ')
+//       if('Andre')
+//         if(// 5)
+//           if({})
+
+
+// Operador Lógico de Negação !
+
+// O operado !, nega uma operação booleana. Ou seje, !true é igaul a false
+
+// Truthy
+// if(!true)      // false
+// if(!1)         // false
+// if(!'')        // true
+// if(!undefined) // true 
+// if(!!)         // true
+// if(!!'')       // false
+
+// if(!possuiGraduacao) {
+//   console.log('Não possui graduação');
+// }
+
+// Operdadores de comparação
+// Sempre retornarão um valor booleano
+// 10 > 5 //  true
+// 5 > 10 //  false
+// 20 < 10 // false
+// 10 <= 10 // true
+// 10 >= 11 // false
+
+// var numero = 165
+// if (numero > 100) {
+//   console.log(`Numero: ${numero}`);
+// } else if (numero >= 100) {
+//   console.log(`${numero} é maior ou igual`);
+// } else if (numero <= 100) {
+//   console.log(`${numero} é menor ou igual`);
+// } else {
+//   console.log(`Não sei`);
+// }
+
+// O == faz uma comparação não tão estrita e o === faz uma comparação estrita, ou seja, o tipo de dado deve ser o mesmo quando usamos ===
+
+10 == '10' // true
+10 == 10 // true
+10 === '10' // false
+10 === 10 // true 
+10 != 15 // true
+10 != '10' // false
+10 !== '10' // true
+
+var x = 10
+console.log(x === 10);
+
+var x = '10'
+console.log(x === 10);
+
+var x = '10'
+console.log(x !== 10);
