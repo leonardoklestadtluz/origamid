38441ffc7ed614e6a06f7f1487bf17bb45e60f2c
leonardoklestadtluz <klestadt@hotmail.com>
2025-08-03 14:07:54 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0205 funções 1

---
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0205-funcoes/script.js
@@ -0,0 +1,80 @@
+// Funções
+
+// Bloco de ´codigo que pode ser executado e reutilizado. Valores podem ser passados por uma função e a mesma retorna outro valor.
+
+// function areaQuadrado(lado) {
+//   return lado * lado
+// }
+// 
+// areaQuadrado(4) // 16
+// areaQuadrado(5) // 25
+// areaQuadrado(2) // 4
+
+//chamada de function declaration
+
+function areaQuadrado(lado) {
+  return lado * lado
+}
+
+console.log(areaQuadrado(3))
+
+
+
+function pi() {
+  return 3.14
+}
+
+var total = 5 * pi()
+console.log(total)
+
+
+// Parâmetros e Argumentos
+
+// Ao CRIAR uma função, você pode definir parâmetros.
+// Ao EXECUTAR uma função, você pode passar argumentos.
+
+// PESO e ALTURA são os argumentos
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2)
+  return imc
+}
+
+console.log(imc (80, 1.80)) // 80 e 1.80 são os argumentos 
+console.log(imc (60, 1.70)) // 60 e 1.70 são os argumentos
+
+// Separar por vírgula cada parâmetro. Você pode definir mais de um parâmetro ou nenhum.
+
+// Parêntese executa a função
+
+function corFavorita(cor) {
+  if (cor === 'Azul') {
+    return 'Você gosta do céu'
+  } else if (cor === 'Verde') {
+    return 'Você gosta de mato'
+  } else {
+    return 'Você não gosta de nada'
+  } 
+}
+console.log(corFavorita()) // retorna 'Você não gosta de nada'
+
+// Se apenas definirmos a função com o function e não executarmos a mesma, nada que estiver dentro dela irá acontecer
+
+
+// Argumentos podem ser funções
+
+// Chamadas de Callback, geralmente são funções que ocorrem após algum evento
+
+addEventListener('click', function() {
+  console.log('Clicou')
+})
+
+// A função possui dois argumentos
+// Primeiro é a string 'click'
+//Segundo é uma função anônima
+
+// Funções anônimas são aquelas em que o nome da função não é definido, escritas como: function() {} ou () => {}
+
+
+
+
+
