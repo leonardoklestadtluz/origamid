d67cc4ee516d1e1dc121b538b940ac1484a72185
leonardoklestadtluz <klestadt@hotmail.com>
2025-08-04 21:41:30 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0205 funções 3

--- CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0205-funcoes/script.js
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0205-funcoes/script.js
@@ -5,7 +5,7 @@
 // function areaQuadrado(lado) {
 //   return lado * lado
 // }
-// 
+//
 // areaQuadrado(4) // 16
 // areaQuadrado(5) // 25
 // areaQuadrado(2) // 4
@@ -18,8 +18,6 @@
 
 // console.log(areaQuadrado(3))
 
-
-
 // function pi() {
 //   return 3.14
 // }
@@ -27,19 +25,18 @@
 // var total = 5 * pi()
 // console.log(total)
 
-
 // Parâmetros e Argumentos
 
 // Ao CRIAR uma função, você pode definir parâmetros.
 // Ao EXECUTAR uma função, você pode passar argumentos.
 
-// PESO e ALTURA são os argumentos
+// PESO e ALTURA são os parâmetros
 // function imc(peso, altura) {
 //   const imc = peso / (altura ** 2)
 //   return imc
 // }
 
-// console.log(imc (80, 1.80)) // 80 e 1.80 são os argumentos 
+// console.log(imc (80, 1.80)) // 80 e 1.80 são os argumentos
 // console.log(imc (60, 1.70)) // 60 e 1.70 são os argumentos
 
 // Separar por vírgula cada parâmetro. Você pode definir mais de um parâmetro ou nenhum.
@@ -53,12 +50,11 @@
 //     return 'Você gosta de mato'
 //   } else {
 //     return 'Você não gosta de nada'
-//   } 
+//   }
 // }
 // console.log(corFavorita()) // retorna 'Você não gosta de nada'
 
 // Se apenas definirmos a função com o function e não executarmos a mesma, nada que estiver dentro dela irá acontecer
-
 
 // Argumentos podem ser funções
 
@@ -73,8 +69,6 @@
 //Segundo é uma função anônima
 
 // Funções anônimas são aquelas em que o nome da função não é definido, escritas como: function() {} ou () => {}
-
-
 
 // Pode ou não retonar um valor
 
@@ -105,12 +99,9 @@
 
 // Cuidado, retornar diferentes tipos de dados em uma mesma função não é uma boa ideia.
 
-
-
 // Escopo
 
 // Variáveis e funções definidas dentro de um bloco {}, não são visíveis fora dele.
-
 
 // function precisoVisitar(paisesVisitados) {
 //   var totalPaises = 193
@@ -119,29 +110,24 @@
 
 // console.log(precisoVisitar(5)) // erro, tiotalPaises não definido
 
-
 // console.log(precisoVisitar(5).totalPaises)
-
 
 // Escopo Léxico
 
 // Funções conseguem acessar variáveis que foram criadas no contexto pai.
 
-
-var profissão = 'Designer'
+var profissão = "Designer";
 
 function dados() {
-  var nome = 'André'
-  var idade = 28
+  var nome = "André";
+  var idade = 28;
   function outrosDados() {
-    var endereco = 'Rio de Janeiro'
-    var idade = 29
-    return `${nome}, ${idade}, ${endereco}, ${profissão}`
+    var endereco = "Rio de Janeiro";
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissão}`;
   }
-  return outrosDados()
+  return outrosDados();
 }
 
-console.log(dados())
-console.log(outrosDados())
-
-
+console.log(dados());
+console.log(outrosDados());
