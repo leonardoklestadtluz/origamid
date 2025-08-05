06cfaff940d4bde112439597c24775b194ac5d9a
leonardoklestadtluz <klestadt@hotmail.com>
2025-08-04 22:20:36 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0206 objetos 1

---
+++ CURSO-JavaScript-Completo-ES6/MODULO-02-JavaScriptParaIniciantes/AULA-0206-objetos/script.js
@@ -0,0 +1,44 @@
+// Objetos
+
+// Conjunto de variáveis e funções, que são chamadas de propriedades e métodos
+
+// var pessoa = {
+//   nome: "André",
+//   idade: 28,
+//   profissão: "Designer",
+//   possuiFaculdade: true,
+// };
+
+// console.log(pessoa);
+// console.log(pessoa.nome); // 'André'
+// console.log(pessoa.possuiFaculdade); // true
+
+// Métodos
+
+// É uma propriedade que poissui uma função no local de seu valor.
+// var quadrado = {
+//   lados: 4,
+//   area: function (lado) {
+//     // os : e a palavra chave function pode ser omitida
+//     return lado * lado;
+//   },
+//   perimetro: function (lado) {
+//     // os : e a palavra chave function pode ser omitida
+//     return this.lados * lado;
+//   },
+// };
+// console.log(`Lados = ${quadrado.lados}`); // 4
+// console.log(`Área = ${quadrado.area(5)}`); // 25
+// console.log(`Perímetro = ${quadrado.perimetro(5)}`); // 20
+
+// Organizar o Código
+
+// Objetos servem para organizar o código em pequenas partes reutilizáveis
+
+Math.PI; // 3,14
+Math.random(); //número aleatório
+
+var pi = Math.PI;
+console.log(pi); // 3,14
+
+// Math é um objeto nativo de JavaScript. Já percebeu que console é um objeto e log() é um método?
