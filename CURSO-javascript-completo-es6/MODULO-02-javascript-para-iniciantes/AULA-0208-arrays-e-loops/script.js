2928903faa581bb5a19104af355a8cd2edc54a63
leonardoklestadtluz <klestadt@hotmail.com>
2025-08-19 15:14:37 -0300

    CURSO javascript completo es6 - MÓDULO 02 javascript para iniciantes, conclusão da AULA 0208 arrays e loops 1

---
+++ CURSO-javascript-completo-es6/MODULO-02-javascript-para-iniciantes/AULA-0208-arrays-e-loops/script.js
@@ -0,0 +1,62 @@
+// Array
+// É um grupo de valores geralmente relacionados. Servem para guardarmos diferentes valores em uma única variável.
+var videoGames = ['Switch', 'PS4' , 'XBox']
+
+console.log(videoGames[2])
+
+// Acesse um elemento da array utilizando [n]
+
+// Métodos e propriedades de uma Array
+var videoGames = ['Switch', 'PS4' , 'XBox']
+
+videoGames.pop() // remove o último item e retorna ele
+videoGames.push('3DS') // adiciona ao final da array
+videoGames.length // 3
+
+// Existem diversos outros métodos,
+// como map, reduce, forEach e 
+// mais que veremos mais à frente
+
+// For Loop
+// Fazem algo repetidamente até que uma condição seja atingida
+
+for (var numero = 0; numero <= 100; numero++) { // retorna de 0 a 9 no console
+  console.log(numero)
+}
+
+// o for loop possuí três partes - início, condição, incremento
+
+// While Loop
+var i = 0
+while (i < 10) { // retorna de 0 a 9 no console
+  console.log(i)
+  i++
+}
+
+// o for loop é mais comum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CURSO-javascript-completo-es6/MODULO-02-javascript-para-iniciantes/AULA-0208-arrays-e-loops/scriptExercicio.js
@@ -0,0 +1,20 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'leonardo'
+console.log(nome.charCodeAt())
+console.log(nome.length)
+console.log(nome.toUpperCase())
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+addEventListener
+onpageswap
+id
+innerText
+outerHTML
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// https://clipboardjs.com/
+
+
+
