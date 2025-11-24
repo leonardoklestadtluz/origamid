7fa1b628ece3e34ace5fdaf0b15a7a2d83b1d66f
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-24 18:12:37 -0300

    CURSO javascript completo es6 - MÓDULO 04 objetos - AULA 0401 constructor function 2, concluída

--- CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0401-constructor-function/script.js
+++ CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0401-constructor-function/script.js
@@ -1,10 +1,61 @@
-function Carro(marcaAtribuida, precoAtribuido) {
-  this.marca = marcaAtribuida
-  this.preco = precoAtribuido
+// function Carro(marcaAtribuida, precoAtribuido) {
+//   this.marca = marcaAtribuida
+//   this.preco = precoAtribuido
+// }
+
+// const honda = new Carro('Honda', 3000)
+// const fiat = new Carro('Fiat', 4000)
+
+// function Carro2(marca, precoInicial) {
+//   const taxa = 1.2
+//   const precoFinal = precoInicial * taxa
+//   this.marca = marca
+//   this.preco = precoFinal
+// }
+
+// const mazda = new Carro2('Mazda', 5000)
+
+
+// EXEMPLINHO REAL
+
+// const Dom = {
+//   seletor: 'li',
+//   element() {
+//     return document.querySelector(this.seletor)
+//   },
+//   ativar() {
+//     this.element().classList.add('ativar')
+//   }
+// }
+
+// Dom.seletor = 'ul'
+// Dom.ativar()
+
+function Dom(seletor) {
+  this.element = function() {
+    return document.querySelector(seletor)
+  }
+
+  this.ativar = function(classe) {
+    this.element().classList.add(classe)
+  }
 }
 
-const honda = new Carro('Honda', 3000)
-const fiat = new Carro('Fiat', 4000)
+const li = new Dom('li')
+const ul = new Dom('ul')
+const lastLi = new Dom('li:last-child')
+lastLi.ativar('ativar')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
