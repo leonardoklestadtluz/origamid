c9452a665c7f15d55cee407ed24137073af3e409
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-24 23:05:14 -0300

    CURSO javascript completo es6 - MÓDULO 04 objetos - AULA 0402, prototype 2, concluída

--- CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0402-prototype/script.js
+++ CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0402-prototype/script.js
@@ -1,15 +1,63 @@
-function Pessoa(nome, idade) {
-  this.nome = nome
-  this.idade = idade
-  this.abracar = function() {
-    return 'Abraçou'
+// function Pessoa(nome, idade) {
+//   this.nome = nome
+//   this.idade = idade
+//   this.abracar = function() {
+//     return 'Abraçou'
+//   }
+// }
+
+// Pessoa.prototype.andar = function() {
+//   return 'Pessoa andou'
+// }
+
+// const leonardo = new Pessoa('Leonardo', 41)
+
+// console.log(Pessoa.prototype)
+
+
+// const pais = 'Brasil'
+// const cidade = new String('Rio')
+
+// const listaAnimais = ['Cachorro', 'Gato', 'Cavalo']
+
+// const lista = document.querySelectorAll('li')
+
+// // Transforma em uma array
+// const listaArray = Array.prototype.slice.call(lista)
+// console.log(listaAnimais)
+
+// const listaArray2 = Array.from(lista)
+// console.log(lista)
+
+const Carro = {
+  marca: 'Ford',
+  preco: 2000,
+  andar() {
+    return true
   }
 }
 
-Pessoa.prototype.andar = function() {
-  return 'Pessoa andou'
-}
 
-const leonardo = new Pessoa('Leonardo', 41)
 
-console.log(Pessoa.prototype)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
