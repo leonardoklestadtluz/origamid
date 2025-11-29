d28152347784a13e5277cc8e3120e5c806e17bbe
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-28 22:06:45 -0300

    CURSO javascript completo es6 - MÓDULO 04 objetos - AULA 0403 Native, Host e User 1, concluída

---
+++ CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0403-native-host-e-user/script.js
@@ -0,0 +1,13 @@
+console.log(typeof Array.from.dsfkgkfdc !== 'undefined')
+
+if(typeof Array.from.dsfkgkfdc !== 'undefined') {
+  console.log('Existe')
+} else {
+  console.log('Não existe')
+}
+
+
+
+
+
+
