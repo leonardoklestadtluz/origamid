a5b20061f04ce103575ee2d48e54c9cf5983954a
leonardoklestadtluz <klestadt@hotmail.com>
2025-11-29 18:54:40 -0300

    CURSO javascript completo es6 - MÓDULO 04 objetos - AULA 0404 string 1, concluída

---
+++ CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0404-string/script.js
@@ -0,0 +1,91 @@
+// const comida = 'pizza'
+
+// const agua = new String('AGUA')
+
+// console.log(comida.length)
+
+// const frase = 'A melhor comida'
+// console.log(frase[frase.length - 1])
+// console.log(frase.charAt(frase.length))
+
+
+// const frase = 'A melhor linguagem é '
+// const linguagem = 'Javascript'
+// const fraseFinal = frase.concat(linguagem, '!!')
+
+
+// const fruta = 'Banana';
+// const listaFrutas = 'Melancia, Banana, Laranja';
+
+// listaFrutas.includes(fruta); // true
+// fruta.includes(listaFrutas); // false
+
+// const fruta = 'Banana';
+// const listaFrutas = 'Melancia, Banana, Laranja';
+
+// listaFrutas.includes(fruta); // true
+// fruta.includes(listaFrutas); // false
+
+
+// const fruta = 'Banana';
+
+// fruta.endsWith('nana'); // true
+// fruta.startsWith('Ba'); // true
+// fruta.startsWith('na'); // false
+
+
+
+// const transacao1 = 'Depósito de cliente';
+// const transacao2 = 'Depósito de fornecedor';
+// const transacao3 = 'Taxa de camisas';
+
+// transacao1.slice(0, 3); // Dep
+// transacao2.slice(0, 3); // Dep
+// transacao3.slice(0, 3); // Tax
+
+// transacao1.slice(12); // cliente
+// transacao1.slice(-4); // ente
+// transacao1.slice(3, 6); // ósi
+
+
+// const linguagem = 'JavaScript';
+// linguagem.substring(3,5); // aS
+// linguagem.substring(0,4); // Java
+// linguagem.substring(4); // Script
+// linguagem.substring(-3); // JavaScript
+
+// const instrumento = 'Guitarra';
+
+// instrumento.indexOf('r'); // 5
+// instrumento.lastIndexOf('r'); // 6
+// instrumento.indexOf('ta'); // 3
+
+
+// const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
+
+// listaPrecos.forEach((preco) => {
+//   console.log(preco.padStart(10, '.'));
+// })
+
+// listaPrecos[0].padStart(10, '.'); // .....R$ 99
+// listaPrecos[0].padEnd(10, '.');   // R$ 99.....
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
