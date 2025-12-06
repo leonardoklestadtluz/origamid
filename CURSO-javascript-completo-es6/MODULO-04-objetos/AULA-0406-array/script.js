95b19183ef63ed47d0284b325b2c6d138506d800
leonardoklestadtluz <klestadt@hotmail.com>
2025-12-05 21:44:26 -0300

    CURSO javascript completo es6 - MÓDULO 04 objetos - AULA 0406 array 2, concluída

--- CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0406-array/script.js
+++ CURSO-javascript-completo-es6/MODULO-04-objetos/AULA-0406-array/script.js
@@ -116,8 +116,128 @@
 // [].reverse()
 // [].reverse() inverte os itens da array e retorna a nova array.
 
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
+// const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// carros.reverse(); // ['Honda', 'VW', 'Fiat', 'Ford'];
+
+// console.log(carros)
+
+
+// [].splice()
+
+// [].splice(index, remover, item1, item2, ...) adiciona valores na array a partir do index. Remove a quantidade de itens que for passada no segundo parâmetro (retorna esses itens).
+
+// const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// carros.splice(1, 0, 'Kia', 'Mustang'); // []
+// carros; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
+
+// carros.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
+// carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
+
+
+// [].copyWithin()
+
+// [].copyWithin(alvo, inicio, final) a partir do alvo, ele irá copiar a array começando do inicio até o final e vai preencher a mesma com essa cópia. Caso omita os valores de início e final, ele irá utilizar como inicio o 0 e final o valor total da array.
+
+// ['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3);[]
+// // ['Item1', 'Item2', 'Item1', 'Item2']
+
+// ['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(-1);
+// // ['Item1', 'Item2', 'Item3', 'Item1']
+
+
+// [].fill();
+// [].fill(valor, inicio, final) preenche a array com o valor, do início até o fim.
+
+// ['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana');
+// ['Banana', 'Banana', 'Banana', 'Banana']
+
+// ['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 2);
+// ['Item1', 'Item2', 'Banana', 'Banana']
+
+// ['Item1', 'Item2', 'Item3', 'Item4'].fill('Banana', 1, 3);
+// ['Item1', 'Banana', 'Banana', 'Item4']
+
+
+// Métodos de Acesso [].concat()
+
+// Os métodos abaixo não modificam a array original, apenas retornam uma array modificada. [].concat() irá concatenar a array com o valor passado.
+
+// const transporte1 = ['Barco', 'Aviao'];
+// const transporte2 = ['Carro', 'Moto'];
+// const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+
+// const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+
+// [].includes(), [].indexOf() e [].lastIndexOf()
+
+// [].includes(valor) verifica se a array possui o valor e retorna true ou false. [].indexOf(valor) verifica se a array possui o valor e retorna o index do primeiro valor na array. Já o [].lastIndexOf(valor) retorna o index do último.
+
+// const linguagens = ['html', 'css', 'js', 'php', 'python', 'js'];
+
+// linguagens.includes('css'); // true
+// linguagens.includes('ruby'); // false
+// linguagens.indexOf('python'); // 4
+// linguagens.indexOf('js'); // 2
+// linguagens.lastIndexOf('js'); // 5
+
+
+
+// [].join()
+
+// [].join(separador) junta todos os valores da array e retorna uma string com eles. Se você passar um valor como parâmetro, este será utilizado durante a junção de cada item da array.
+
+// const linguagens = ['html', 'css', 'js', 'php', 'python'];
+// linguagens.join(); // 'html,css,js,php,python'
+// linguagens.join(' '); // 'html css js php python'
+// linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
+
+// let htmlString = '<h2>Título Principal</h2>'
+// htmlString = htmlString.split('h2');
+// ['<', '>Título Principal</', '>']
+// htmlString = htmlString.join('h1');
+// <h1>Título Principal</h1>
+
+
+// [].slice()
+
+// [].slice(inicio, final) retorna os itens da array começando pelo início e indo até o valor de final.
+
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+linguagens.slice(3); // ['php', 'python']
+linguagens.slice(1, 4); // ['css', 'js', 'php']
+
+const cloneLinguagens = linguagens.slice();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
